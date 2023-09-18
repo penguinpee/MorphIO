3e18a42fd034c7bab8e001d4245689575e859d25
Sandro <devel@penguinpee.nl>
2023-09-18 13:50:37 +0200

    Allow for usage of external ghc_filesystem

--- CMake/MorphIOConfig.cmake
+++ CMake/MorphIOConfig.cmake
@@ -2,5 +2,6 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(gsl-lite)
 find_dependency(HighFive)
+find_dependency(ghc_filesystem)
 
 include("${CMAKE_CURRENT_LIST_DIR}/MorphIOTargets.cmake")
